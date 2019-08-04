dab4290dd1a12a79dbedf57093ca459fa6294a9a
GlowzSucks <53585351+GlowzSucks@users.noreply.github.com>
2019-08-04 18:55:32 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,31 +8,23 @@ import os
 bot = commands.Bot(command_prefix="$")
 
 
-
 #PRINT THE DISCORD BOT'S NAME WHEN IT'S READY
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is now running!")
 
 
-
-	
 @bot.command()
-async def greet(msg,user:discord.Member):
-    await msg.send(f"Hello there {user}")
-    
-    
-    
-@bot.command(aliases=['echo','copy','say'])
+async def greet(msg, *users: discord.Member):
+    names=[user.name for user in users]
+    await msg.send(f"Hello there {' '.join(names)}")
+
+@bot.command()
 async def repeat(msg,*,message=None):
     if message == None:
-        await msg.send("Please enter a message to repeat")
-
+        await msg.send("Enter a message to repeat")
+    
     else:
         await msg.send(f"{msg.author.name}: {message}")
-  
-  
-  
-#IF YOU WISH TO HOST YOUR CODE PUBLICALLY HIDE YOUR TOKEN VIA METHOD BELOW
-#YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] ON HEROKU Config Vars
+
 bot.run(os.environ['BOT_TOKEN'])
