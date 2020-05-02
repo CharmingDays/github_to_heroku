727e34e0ba1e140e4ab9f8e3921fd93cef2fd241
CharmingDays <nawhpongmaji1@gmail.com>
2020-05-01 22:26:34 -0500

    Converted it to rewrite
    Converted the bot commands into rewrite for simpler usage of the commands

--- bot.py
+++ bot.py
@@ -1,42 +1,30 @@
-import discord
-from discord.ext import commands
-import asyncio
+
+
 import os
+import discord
+import random
+from discord.ext import commands
 
+bot=commands.Bot(command_prefix='.')
 
-#GIVE YOUR BOT A PREFIX; mine is a.
-bot = commands.Bot(command_prefix="s.")
-
-
-
-#PRINT THE DISCORD BOT'S NAME WHEN IT'S READY
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} is now running!")
+    print(bot.user.name, "is ready!")
+
+
+@bot.command()
+async def say(msg,*,message):
+    #Repeats whatever user types after the .say
+    return await msg.send(message)
 
 
 
-	
-#A SIMPLE TEST COMMAND
-@bot.command(pass_context=True)
-async def hi(ctx):
-	"""
-	Sends Hi with the user of the user that used the command
-	"""
-	await bot.say(f"Hello there{ctx.message.author.display_name}")
-  
+@bot.command()
+async def greet(msg):
+    #Greets the user with random begining everytime
+    greetings=['Hello there','Hi there','Hiya','Hello','Hi']
+    return await msg.send(f"{random.choice(greetings)} {msg.author.name}")
 
 
-@bot.command(pass_context=True,name='copy')
-async def _copy(ctx,*,msg):
-	"""
-	Repearts what ever the user tpyes after s.copy
-	"""
-	await bot.say(msg) #Send the messages
 
-  
-  
-  
-#IF YOU WISH TO HOST YOUR CODE PUBLICALLY HIDE YOUR TOKEN VIA METHOD BELOW
-#YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] ON HEROKU Config Vars
-bot.run(os.environ['BOT_TOKEN'])
+bot.run(os.getenv('BOT_TOKEN')) #NOTE: Replace the word BOT_TOKEN with the name of your Config Var name representing your bot token
